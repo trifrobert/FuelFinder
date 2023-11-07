fc12c4460eabdd575d3f142fc2826fb6b541db48
cristinavasiliu <cristina.vasiliu@student.upt.ro>
2023-11-07 16:22:11 +0200

    Changes in the App.js file. Introduced app logo

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+//framework was taken from net ninja - react native tutorial for begginers 
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('/assets/fuel_logo.png')} 
+      style={styles.image}/> 
+      <Text style={styles.title}>
+        <Text style={styles.fuelWord}>Fuel</Text>
+        <Text style={styles.finderWord}>Finder</Text>
+      </Text>
+      <Text style={styles.slogan}>
+        <Text style={styles.description}> Fast, Affordable Fuel Right Next To You!</Text>
+      </Text>
+      {/* Your other content */}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +27,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start', // Vertically align content at the top
+    paddingTop: 100, // Add padding to the top
   },
+
+  title: {
+    fontSize: 24, // You can adjust the font size as needed
+    textAlign: 'center', // Center the text horizontally
+  },
+
+  fuelWord: {
+    fontWeight : 'bold',
+    color : 'rgb(8,8,8)'
+  },
+
+  finderWord: {
+    color: 'rgb(33,33,33)'
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+  },
+
+  slogan: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginVertical: 10,
+  }
+
+ 
+
+
 });
